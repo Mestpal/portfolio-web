e67a7bd7781ada40a04824abf5191501bdaf9bc8
Manuel Estevez <manuestpal@gmail.com>
2023-11-21 00:20:01 +0100

    main component - 1st section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,40 @@
-PEPE
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section 
+        id="introPage"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-18">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="text-violet-400">Manuel</span> Estevez Palencia<br>
+                A Full Stak <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My favourite technologies stock includes: JavaScript, frameworks like VUE or Nuxt, <br>
+                component libraries like Vuetify, or style librarieslike Bulma
+            </p>
+            <a 
+                class="blue-shadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins 
+                relative overflow-hidden px-4 py-3 group rounded-full bg-white text-slate-950
+                cursor-pointer"
+                href="#"
+                target="_blank" 
+            >
+                <div 
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rArr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-item center">
+            <img
+                src={"images/profile.jpg"}
+                alt="Profile Image"
+                class="object-cover z-[2] max-h-[70vh]"
+            >
+        </div>
+    </section>
+</main>
