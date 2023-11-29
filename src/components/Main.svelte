89acad3120015633997f4d11b814acd151b26e05
Manuel Estevez <manuestpal@gmail.com>
2023-11-29 18:54:33 +0100

    about section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,6 +6,11 @@
         {name: 'project 2', icon: 'fa-solid fa-list-check'},
         {name: 'project 3', icon: 'fa-solid fa-list-check'},
     ]
+
+    let benefits = [
+        {name: 'Good solving problems', description: 'description'},
+        {name: 'I work well in groups and alone', description: 'description'},
+    ]
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -54,7 +59,7 @@
             </h3>
             <a href="https://youtube.com/shorts/EZkiplhQiMs?feature=shared"
             target="_blank" class="mx-auto px-4 py-2 rounded-md border border-solid border-white 
-            flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+            flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200 z-[10]"
             >
                 <i class="fa-regular fa-circle-play"></i>
                 <p>Watch the video</p>
@@ -73,7 +78,40 @@
         </div>
     </section>
 
-    <section id="about">
-
+    <section 
+        id="about" 
+        class="py-20 pt-10 lg:pt-16 lg:py-32 
+        flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+        >
+        <div 
+        class="flex flex-col gap-2 text-center relative 
+        before:absolute before:top-0 before:left-0 
+        before:w-2/3 before:h-1.5 before:bg-violet-700
+        after:absolute after:bottom-0 after:right-0 
+        after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                Want to know more?
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am ...</p>
+        <div class="flex flex-col gap-20 w-full-mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+            <div class="flex gap-6 sm:gap-8">
+                <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500">
+                    0{index + 1}
+                </p>
+                <div class="flex flex-col gap-6 sm:gap-8">
+                    <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                        {benefit.name}
+                    </h3>
+                    <p>{benefit.description}</p>
+                </div>
+            </div>
+            {/each}
+        </div>
     </section>
 </main>
