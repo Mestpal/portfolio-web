07525b200ccf1aa4dab4e7a7fdc96b9742bc5458
Manuel Estevez <manuestpal@gmail.com>
2024-01-07 12:29:27 +0100

    Add Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-content items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> example@test.com</p>
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a href="#" class="text-violet-400" target="_blank">
+                <span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a href="#" class="text-violet-400" target="_blank">
+                <span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span>
+            </a>
+        </p>
+    </div>
+</footer>
